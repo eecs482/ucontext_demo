d64a9223a0167b70e81315e240b4071f8b100f42
Yilin Yang <yiliny@umich.edu>
2020-10-01 18:54:19 -0400

    Fix init_context(); eject yeet_context()
    Clean up init_context() to only populate the stack pointer/etc. when
    invoking makecontext(). ucontext manpages say that the stack
    pointer/etc. is only necessary when invoking makecontext().
    
    yeet_context() was An Impostor.
    1 Impostor remains.

--- Makefile
+++ Makefile
@@ -1,2 +1,7 @@
+.PHONY: clean
+
 context.out: context.cpp
 	g++ -Wall -pedantic -g3 -std=c++14 -pthread -o $@ $^
+
+clean:
+	rm -rf *.out *.o
--- context.cpp
+++ context.cpp
@@ -8,7 +8,6 @@ typedef void (*thread_startfunc_t) (void *);
 static const unsigned int STACK_SIZE = 262144;
 
 ucontext_t* init_context(thread_startfunc_t func, void*);
-void yeet_context(ucontext_t* ucp);
 
 int global = 0;
 
@@ -20,10 +19,12 @@ void print_args(intptr_t arg) {
 
   arg += global;
   global += 1;
-  cout << "after incr: " << arg << endl;
+  cout << "arg after incr by global: " << arg << endl;
 
   cout << "finish print_args" << endl;
-  setcontext(main_c);
+  if (arg == 1) {
+    setcontext(main_c);
+  }
 }
 
 int main() {
@@ -32,28 +33,19 @@ int main() {
   c2 = init_context((thread_startfunc_t) print_args, (void*) 2);
   swapcontext(main_c, c1);
   swapcontext(main_c, c2);
-  yeet_context(c1);
-  yeet_context(c2);
-  yeet_context(main_c);
   cout << "done" << endl;
 }
 
 ucontext_t* init_context(thread_startfunc_t func, void* arg) {
   ucontext_t* ucp = new ucontext_t;
-  char* stack = new char[STACK_SIZE];
-  ucp->uc_stack.ss_sp = stack;
-  ucp->uc_stack.ss_size = STACK_SIZE;
-  ucp->uc_stack.ss_flags = 0;
+  getcontext(ucp);  // initialize ucontext, as required by interface
   ucp->uc_link = nullptr;
-  getcontext(ucp); // why do we need this? why? wHY???
   if (func) {
+    char* stack = new char[STACK_SIZE];
+    ucp->uc_stack.ss_sp = stack;
+    ucp->uc_stack.ss_size = STACK_SIZE;
+    ucp->uc_stack.ss_flags = 0;
     makecontext(ucp, (void (*)()) func, 1, arg);
   }
   return ucp;
-}
-
-void yeet_context(ucontext_t* ucp) {
-  cout << "this context empty. yEET" << endl;
-  delete[] (char*)ucp->uc_stack.ss_sp;
-  delete ucp;
 }
